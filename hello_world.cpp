4b10d940db4b73ef87229d2f6c87abeb5742c697
Veronikaaaaaaaaaaaaaa <nechaeva.veronika.04@gmail.com>
2026-05-06 22:45:38 +0300

    apply Mozilla code style via clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std::string name;
-    // ask user for their name
-    std::cin >> name;
-    // greeting the user
-    std::cout << "Hello world from @" << name << std::endl;
+int
+main()
+{
+  std::string name;
+  // ask user for their name
+  std::cin >> name;
+  // greeting the user
+  std::cout << "Hello world from @" << name << std::endl;
 }
 
 // ask user for their name - please enter your name
